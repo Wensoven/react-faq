6a472ab0ca61920cfbaa19cf2038ee7ae6957368
Wensoven <86717798+Wensoven@users.noreply.github.com>
2022-03-03 01:10:07 +0200

    First Commit

---
+++ 1701682019_Martin_Georgiev/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: 'Как да се регистрирам?',
+    info:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+  },
+  {
+    id: 2,
+    title: 'Как да потвърдя своята регистрация?',
+    info:
+      'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+  },
+  {
+    id: 3,
+    title: 'Как мога да се свържа с вашата компания?',
+    info:
+      'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+  },
+  {
+    id: 4,
+    title: 'Мога ли да имам повече от един създаден акаунт?',
+    info:
+      'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 5,
+    title: 'Забравих паролата си, възможно ли е да получа нова?',
+    info:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+  },
+]
+export default questions
